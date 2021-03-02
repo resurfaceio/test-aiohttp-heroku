1184a25eb85202d493796431e1edca7301f8d0d8
Anyesh <sthacruz@gmail.com>
2021-03-02 19:25:50 +0545

    Update root GQL route

--- hackernews/routes.py
+++ hackernews/routes.py
@@ -3,15 +3,17 @@ import pathlib
 import aiohttp_cors
 
 from hackernews.api.views import gqil_view, gql_view
+from hackernews.views import ping
 
 PROJECT_PATH = pathlib.Path(__file__).parent.parent
 
 
 def init_routes(app, cors):
     app.router.add_route("*", "/graphiql", gqil_view, name="graphiql")
+    app.router.add_route("*", "/ping", ping, name="ping")
 
     resource = cors.add(
-        app.router.add_resource("/graphql"),
+        app.router.add_resource(""),
         {
             "*": aiohttp_cors.ResourceOptions(
                 expose_headers="*",
