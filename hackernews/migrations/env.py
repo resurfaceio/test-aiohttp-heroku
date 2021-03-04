1bc51d8baa39829581b4b40b8229d83c15fa182a
Anyesh <sthacruz@gmail.com>
2021-03-04 13:39:56 +0545

    Change posgres URL

--- hackernews/migrations/env.py
+++ hackernews/migrations/env.py
@@ -1,5 +1,6 @@
 from __future__ import with_statement
 
+import os
 from logging.config import fileConfig
 
 # import here you table files for autodetect your tables by the alembic
@@ -11,6 +12,9 @@ from sqlalchemy import engine_from_config, pool
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+section = config.config_ini_section
+config.set_section_option(section, "DATABASE_URL", os.environ.get("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
